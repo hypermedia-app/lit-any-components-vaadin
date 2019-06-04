c36af9f6d916dbf25e799d147fdef022a11d8f8a
tpluscode <tpluscode@users.noreply.github.com>
2019-06-04 13:34:21 +0200

    reduce nesting of textbox tests

--- test/textbox.test.ts
+++ test/textbox.test.ts
@@ -3,99 +3,97 @@ import '@vaadin/vaadin-text-field/vaadin-text-field'
 import components from '..'
 import render from './helper/render'
 
-describe('vaadin components', () => {
+describe('textbox', () => {
     let opts
 
-    describe('textbox', () => {
-        describe('single line', () => {
-            beforeEach(() => {
-                opts = {
-                    type: 'single line',
-                }
-            })
-
-            it('should mark required when field is required', async () => {
-                // given
-                const field = {
-                    required: true,
-                }
-
-                // when
-                const textbox = components.textbox(opts)
-                const el = await render(textbox, field)
-
-                // then
-                expect(el.getAttribute('required')).to.be.not.null
-            })
-
-            it('should set field title as label', async () => {
-                // given
-                const field = {
-                    title: 'user name',
-                }
-
-                // when
-                const textbox = components.textbox(opts)
-                const el = await render(textbox, field)
-
-                // then
-                expect(el.label).to.equal('user name')
-            })
-
-            it('should be required if field is required', async () => {
-                // given
-                const field = {
-                    title: 'user name',
-                    required: true,
-                }
-
-                // when
-                const textbox = components.textbox(opts)
-                const el = await render(textbox, field)
-
-                // then
-                expect(el.required).to.be.true
-            })
+    describe('single line', () => {
+        beforeEach(() => {
+            opts = {
+                type: 'single line',
+            }
         })
 
-        describe('multi line', () => {
-            before(async () => {
-                await import('@vaadin/vaadin-text-field/vaadin-text-area')
-            })
+        it('should mark required when field is required', async () => {
+            // given
+            const field = {
+                required: true,
+            }
 
-            beforeEach(() => {
-                opts = {
-                    type: 'multi line',
-                }
-            })
+            // when
+            const textbox = components.textbox(opts)
+            const el = await render(textbox, field)
 
-            it('should render a textarea', async () => {
-                // given
-                const field = {
-                }
+            // then
+            expect(el.getAttribute('required')).to.be.not.null
+        })
 
-                // when
-                const textbox = components.textbox(opts)
-                const el = await render(textbox, field)
+        it('should set field title as label', async () => {
+            // given
+            const field = {
+                title: 'user name',
+            }
 
-                // then
-                expect(el.tagName).to.match(/vaadin-text-area/i)
-            })
+            // when
+            const textbox = components.textbox(opts)
+            const el = await render(textbox, field)
 
-            it('should be required if field is required', async () => {
-                // given
-                const field = {
-                    title: 'user name',
-                    required: true,
-                }
+            // then
+            expect(el.label).to.equal('user name')
+        })
 
-                // when
-                const textbox = components.textbox(opts)
-                const el = await render(textbox, field)
+        it('should be required if field is required', async () => {
+            // given
+            const field = {
+                title: 'user name',
+                required: true,
+            }
 
-                // then
-                expect(el.required).to.be.true
-            })
+            // when
+            const textbox = components.textbox(opts)
+            const el = await render(textbox, field)
+
+            // then
+            expect(el.required).to.be.true
+        })
+    })
+
+    describe('multi line', () => {
+        before(async () => {
+            await import('@vaadin/vaadin-text-field/vaadin-text-area')
+        })
+
+        beforeEach(() => {
+            opts = {
+                type: 'multi line',
+            }
+        })
+
+        it('should render a textarea', async () => {
+            // given
+            const field = {
+            }
+
+            // when
+            const textbox = components.textbox(opts)
+            const el = await render(textbox, field)
+
+            // then
+            expect(el.tagName).to.match(/vaadin-text-area/i)
+        })
+
+        it('should be required if field is required', async () => {
+            // given
+            const field = {
+                title: 'user name',
+                required: true,
+            }
+
+            // when
+            const textbox = components.textbox(opts)
+            const el = await render(textbox, field)
+
+            // then
+            expect(el.required).to.be.true
         })
     })
 })
