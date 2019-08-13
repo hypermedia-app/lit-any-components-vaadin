6fedae39214b3007cb2414db00d445a648931a9c
tpluscode <tpluscode@users.noreply.github.com>
2019-08-13 19:53:24 +0200

    style: tweak linting and formatting

--- test/helper/render.ts
+++ test/helper/render.ts
@@ -1,10 +1,16 @@
-import { render } from 'lit-html'
+import { render as litRender } from 'lit-html'
 import { fixture } from '@open-wc/testing'
 
-export default async function(componentFactory, field, id = 'id', value = '', setter = () => {}) {
+export default async function render(
+  componentFactory,
+  field,
+  id = 'id',
+  value = '',
+  setter = () => {},
+) {
   const container = await fixture('<div></div>')
 
-  render(await componentFactory(field, id, value, setter), container)
+  litRender(await componentFactory(field, id, value, setter), container)
 
   return container.children[0]
 }
